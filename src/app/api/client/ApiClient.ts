314451f9ffd39d4f4799c45ab0eef912d999a3bb
Cris <chris_49git@outlook.com>
2025-05-29 12:34:16 -0600

    Actualizar la URL base del cliente API y corregir el token JWT en el interceptor

--- src/app/api/client/ApiClient.ts
+++ src/app/api/client/ApiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://192.168.1.65:8081/ftp",
+  baseURL: "http://192.168.0.200:8081/ftp",
   headers: {
     "Content-Type": "application/json",
   },
@@ -10,7 +10,7 @@ const apiClient = axios.create({
 // Interceptor para agregar el token JWT
 apiClient.interceptors.request.use((config) => {
   const token =
-    "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwMTIxIiwiaWRVc2VyIjoxLCJpZFBlcnNvbmFsSW5mbyI6MSwicm9sZSI6IkFkbWluIiwicGVyc29uYWxQYXRoIjoiL2hvbWUvYWRtaW4iLCJpYXQiOjE3NDgzOTQ1MzYsImV4cCI6MTc0ODQzMDUzNn0.G1JspBdZccj2C6ErXS_do3KQJeixYnlexDsmvSGE55E";
+    "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwMTIxIiwiaWRVc2VyIjoxLCJpZFBlcnNvbmFsSW5mbyI6MSwicm9sZSI6IkFkbWluIiwicGVyc29uYWxQYXRoIjoiL2hvbWUvYWRtaW4iLCJpYXQiOjE3NDg0NTI0MzYsImV4cCI6MTc0ODQ4ODQzNn0.GXZXKTf3kGdng2P-gu1Ox67TcSiJxGV77zciHf0HEMk";
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
